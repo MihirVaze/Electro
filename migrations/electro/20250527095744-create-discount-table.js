370894cd59cf970cffd29686ce191823bb27c916
Vandith Kadamba <vandith.kadamba@coditas.com>
2025-05-27 15:28:21 +0530

    Added discount migration file

---
+++ migrations/electro/20250527095744-create-discount-table.js
@@ -0,0 +1,96 @@
+module.exports = {
+    async up({ context }) {
+        const { queryInterface, Sequelize, schema } = context;
+        const { DataTypes } = Sequelize;
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.createTable(
+                { tableName: 'Discount', schema },
+                {
+                    id: {
+                        type: DataTypes.UUID,
+                        defaultValue: DataTypes.UUIDV4,
+                        primaryKey: true,
+                    },
+                    clientId: {
+                        type: DataTypes.INTEGER,
+                        allowNull: false,
+                    },
+                    type: {
+                        type: DataTypes.ENUM('increment', 'decrement', 'none'),
+                        allowNull: false,
+                    },
+                    value: {
+                        type: DataTypes.INTEGER,
+                        allowNull: false,
+                    },
+                    isDeleted: {
+                        type: DataTypes.BOOLEAN,
+                        defaultValue: false,
+                    },
+                    deletedBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    restoredBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    createdBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    updatedBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    deletedAt: {
+                        type: DataTypes.DATE,
+                    },
+                    restoredAt: {
+                        type: DataTypes.DATE,
+                    },
+                },
+                { transaction },
+            );
+
+            await transaction.commit();
+        } catch (error) {
+            console.log(error);
+            console.log('..........Rolling Back Transaction..........');
+            await transaction.rollback();
+            console.log('.........Transaction Rolled Back..........');
+            throw error;
+        }
+    },
+
+    async down({ context }) {
+        const { queryInterface, Sequelize, schema } = context;
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.dropTable({ tableName: 'Discount', schema });
+
+            await transaction.commit();
+        } catch (error) {
+            console.log(error);
+            console.log('..........Rolling Back Transaction..........');
+            await transaction.rollback();
+            console.log('.........Transaction Rolled Back..........');
+            throw error;
+        }
+    },
+};
