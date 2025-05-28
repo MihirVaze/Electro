b3687f84fe7646946eca122d4a181c6c5e7bebf9
Behla Burhani <behla.burhani@coditas.com>
2025-05-28 10:55:11 +0530

    Added Routes to delete or edit user also Client Admin can be created with the sme onboard function

--- src/feature-modules/user/user.routes.ts
+++ src/feature-modules/user/user.routes.ts
@@ -1,6 +1,6 @@
 import { CustomRouter } from '../../routes/custom.router';
 import { Route } from '../../routes/routes.types';
-import { CanRegister } from '../../utility/createUsersPermission';
+import { HasPermission } from '../../utility/usersPermissions';
 import { ResponseHandler } from '../../utility/response-handler';
 import { validate } from '../../utility/validate';
 import userService from './user.service';
@@ -20,7 +20,7 @@ router.post(
                 const creatorRoleId = req.payload.roleId;
                 const { user, roles } = req.body;
 
-                const canRegister = CanRegister(creatorRoleId, roles, schema);
+                const canRegister = HasPermission(creatorRoleId, roles, schema);
                 if (!canRegister) throw { status: 403, message: 'FORBIDDEN' };
 
                 const result = await userService.onBoardUser(
@@ -29,6 +29,97 @@ router.post(
                     schema,
                 );
                 res.send(new ResponseHandler(result.responses));
+            } catch (e) {
+                next(e);
+            }
+        },
+    ],
+    {
+        is_protected: true,
+        has_Access: [
+            'superadmin',
+            'client_manager',
+            'client_admin',
+            'state_manager',
+            'district_manager',
+            'city_manager',
+        ],
+    },
+);
+
+router.patch(
+    '/user/:id?',
+    [
+        async (req, res, next) => {
+            try {
+                const schema = req.payload.schema;
+                const editorRoleId = req.payload.roleId;
+                const user = req.body;
+                user.id = req.params.id || req.payload.id;
+
+                // if we are taking id from params that means someone else is editing the user so we need to know if they have permition
+                if (req.params.id) {
+                    const userRoles = (
+                        await userService.getUserRoles(
+                            { userId: user.id },
+                            schema,
+                        )
+                    )
+                        .map((e) => e.id)
+                        .filter((e): e is string => !!e);
+                    const canUpdate = HasPermission(
+                        editorRoleId,
+                        userRoles,
+                        schema,
+                    );
+                    if (!canUpdate) throw { status: 403, message: 'FORBIDDEN' };
+                }
+
+                const result = await userService.updateUser(user, schema);
+                res.send(new ResponseHandler(result));
+            } catch (e) {
+                next(e);
+            }
+        },
+    ],
+    {
+        is_protected: true,
+        has_Access: [
+            'superadmin',
+            'client_manager',
+            'client_admin',
+            'state_manager',
+            'district_manager',
+            'city_manager',
+            'worker',
+            'service_worker',
+        ],
+    },
+);
+
+router.del(
+    '/:id',
+    [
+        async (req, res, next) => {
+            try {
+                const userId = req.params.id;
+                const schema = req.payload.schema;
+                const deletorRoleId = req.payload.roleId;
+                const userRoles = (
+                    await userService.getUserRoles({ userId }, schema)
+                )
+                    .map((e) => e.id)
+                    .filter((e): e is string => !!e);
+
+                const canUpdate = HasPermission(
+                    deletorRoleId,
+                    userRoles,
+                    schema,
+                );
+                if (!canUpdate) throw { status: 403, message: 'FORBIDDEN' };
+
+                const result = await userService.deleteUser(userId, schema);
+                res.send(new ResponseHandler(result));
             } catch (e) {
                 next(e);
             }
