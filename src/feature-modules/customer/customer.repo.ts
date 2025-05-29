04e527c8c0b64ef95b070608df7703184bd5d60e
Pranjal Lohi <pranjal.lohi@coditas.com>
2025-05-29 23:21:42 +0530

    Added CustomerMeter Module

--- src/feature-modules/customer/customer.repo.ts
+++ src/feature-modules/customer/customer.repo.ts
@@ -1,7 +1,7 @@
 import { SchemaName } from '../../utility/umzug-migration';
 import { FindOptions, UpdateOptions } from 'sequelize';
-import { Customer } from './customer.type';
-import { CustomerSchema } from './customer.schema';
+import { Customer, CustomerMeter } from './customer.type';
+import { CustomerMeterSchema, CustomerSchema } from './customer.schema';
 
 class CustomerRepo {
     public async create(Customer: Customer, schema: SchemaName) {
@@ -26,6 +26,49 @@ class CustomerRepo {
 
     public async delete(options: UpdateOptions<Customer>, schema: SchemaName) {
         return CustomerSchema.schema(schema).update(
+            { isDeleted: true },
+            options,
+        );
+    }
+
+    //CustomerMeter
+    public async createCustomerMeter(
+        customerMeter: CustomerMeter,
+        schema: SchemaName,
+    ) {
+        return CustomerMeterSchema.schema(schema).create(customerMeter);
+    }
+
+    public async getCustomerMeter(
+        options: FindOptions<CustomerMeter>,
+        schema: SchemaName,
+    ) {
+        return CustomerMeterSchema.schema(schema).findOne(options);
+    }
+
+    public async getAllCustomerMeter(
+        options: FindOptions<CustomerMeter>,
+        schema: SchemaName,
+    ) {
+        return CustomerMeterSchema.schema(schema).findAndCountAll(options);
+    }
+
+    public async updateCustomerMeter(
+        customerMeter: Partial<CustomerMeter>,
+        options: UpdateOptions<CustomerMeter>,
+        schema: SchemaName,
+    ) {
+        return CustomerMeterSchema.schema(schema).update(
+            customerMeter,
+            options,
+        );
+    }
+
+    public async deleteCustomerMeter(
+        options: UpdateOptions<CustomerMeter>,
+        schema: SchemaName,
+    ) {
+        return CustomerMeterSchema.schema(schema).update(
             { isDeleted: true },
             options,
         );
