9d88fd1b0caa2fb9e8c72013cacf26c32e63e436
Pranjal Lohi <pranjal.lohi@coditas.com>
2025-05-29 23:50:15 +0530

    added customerMeter migrations

---
+++ migrations/client/20250529181402-create-customerMeter-table.js
@@ -0,0 +1,103 @@
+module.exports = {
+    async up({ context }) {
+        const { queryInterface, Sequelize, schema } = context;
+        const { DataTypes } = Sequelize;
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.createTable(
+                { tableName: 'CustomerMeter', schema },
+                {
+                    id: {
+                        type: DataTypes.UUID,
+                        defaultValue: DataTypes.UUIDV4,
+                        primaryKey: true,
+                    },
+                    userId: {
+                        type: DataTypes.UUID,
+                        allowNull: false,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    meterId: {
+                        type: DataTypes.UUID,
+                        allowNull: false,
+                        references: {
+                            model: MeterSchema,
+                            key: 'id',
+                        },
+                    },
+                    isDeleted: {
+                        type: DataTypes.BOOLEAN,
+                        defaultValue: false,
+                    },
+                    deletedBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    restoredBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    createdBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    updatedBy: {
+                        type: DataTypes.UUID,
+                        references: {
+                            model: 'User',
+                            key: 'id',
+                        },
+                    },
+                    deletedAt: {
+                        type: DataTypes.DATE,
+                    },
+                    restoredAt: {
+                        type: DataTypes.DATE,
+                    },
+                },
+                { transaction },
+            );
+
+            await transaction.commit();
+        } catch (error) {
+            console.log(error);
+            console.log('..........Rolling Back Transaction..........');
+            await transaction.rollback();
+            console.log('.........Transaction Rolled Back..........');
+            throw error;
+        }
+    },
+
+    async down({ context }) {
+        const { queryInterface, Sequelize, schema } = context;
+        const transaction = await queryInterface.sequelize.transaction();
+
+        try {
+            await queryInterface.dropTable({
+                tableName: 'CustomerMeter',
+                schema,
+            });
+
+            await transaction.commit();
+        } catch (error) {
+            console.log(error);
+            console.log('..........Rolling Back Transaction..........');
+            await transaction.rollback();
+            console.log('.........Transaction Rolled Back..........');
+            throw error;
+        }
+    },
+};
