a0c792010c5c26f0378a17605570f1e70fa6a199
Vandith Kadamba <vandith.kadamba@coditas.com>
2025-06-01 23:43:08 +0530

    Added clientBill migration, types, schema and repo

---
+++ src/feature-modules/billing/clientBill/clientBill.repo.ts
@@ -0,0 +1,48 @@
+import { FindOptions, UpdateOptions } from 'sequelize';
+import { SchemaName } from '../../../utility/umzug-migration';
+import {
+    ClientBill,
+    CreateClientBill,
+    UpdateClientBill,
+} from './clientBill.types';
+import { ClientBillSchema } from './clientBill.schema';
+
+class ClientBillRepo {
+    public async getAll(
+        clientBill: FindOptions<ClientBill>,
+        schema: SchemaName,
+    ) {
+        return ClientBillSchema.schema(schema).findAndCountAll(clientBill);
+    }
+
+    public async getOne(
+        clientBill: FindOptions<ClientBill>,
+        schema: SchemaName,
+    ) {
+        return ClientBillSchema.schema(schema).findOne(clientBill);
+    }
+
+    public async create(clientBill: CreateClientBill, schema: SchemaName) {
+        return ClientBillSchema.schema(schema).create(clientBill);
+    }
+
+    public async update(
+        clientBill: Partial<UpdateClientBill>,
+        options: UpdateOptions<CreateClientBill>,
+        schema: SchemaName,
+    ) {
+        return ClientBillSchema.schema(schema).update(clientBill, options);
+    }
+
+    public async delete(
+        options: UpdateOptions<CreateClientBill>,
+        schema: SchemaName,
+    ) {
+        return ClientBillSchema.schema(schema).update(
+            { isDeleted: true },
+            options,
+        );
+    }
+}
+
+export default new ClientBillRepo();
