1f0eff8847caf17ad984e5bf273d7c9db8e00cf0
Sweta <sweta.tiwari@coditas.com>
2025-06-02 15:26:49 +0530

    changes 1

--- src/feature-modules/meter/meter.type.ts
+++ src/feature-modules/meter/meter.type.ts
@@ -1,12 +1,12 @@
-import { z } from 'zod';
+import { boolean, z } from 'zod';
 import { ZBaseSchema } from '../../utility/base-schema';
 
 export const Zmeter = ZBaseSchema.partial().extend({
     name: z.string().trim().min(1, { message: 'Name cannot be blank' }),
     image: z.string().nonempty(),
-    basePrice: z.number().positive(),
-    pricePerUnit: z.number().positive(),
-    requiredPhotos: z.number().positive(),
+    basePrice: z.coerce.number().positive(),
+    pricePerUnit: z.coerce.number().positive(),
+    requiredPhotos: z.coerce.number().positive(),
 });
 
 export type Meter = z.infer<typeof Zmeter>;
@@ -17,8 +17,6 @@ export const ZFilterMeter = z.object({
     pricePerUnit: z.coerce.number().optional(),
     requiredPhotos: z.coerce.number().optional(),
     isDeleted: z.coerce.boolean().optional(),
-    limit: z.coerce.number().min(1),
-    page: z.coerce.number().min(1),
 });
 export type ZFilterMeterType = z.infer<typeof ZFilterMeter>;
 
@@ -31,3 +29,26 @@ export const ZUpdateMeter = z.object({
 });
 
 export type Update = z.infer<typeof ZUpdateMeter>;
+
+export const ZValidateCreateMeter = z.object({
+    body: Zmeter.omit({
+        image: true,
+    }),
+});
+
+export const ZValidateUpdateMeter = z.object({
+    body: Zmeter.optional(),
+});
+
+export const ZValidateMeterId = z.object({
+    params: Zmeter.pick({
+        id: true,
+    }),
+});
+
+export const ZValidateGetPaginatedMeters = z.object({
+    query: ZFilterMeter.extend({
+        limit: z.coerce.number().optional(),
+        page: z.coerce.number().optional(),
+    }),
+});
